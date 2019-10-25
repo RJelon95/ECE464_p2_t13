43f54ae6c54511c7efe818a4698b72b78d73fe4d
JBRUU <47766826+JBRUU@users.noreply.github.com>
2019-10-25 14:34:46 -0500

    Update TV_A.py
    updated to match all project requirements

--- TV_A.py
+++ TV_A.py
@@ -1,43 +1,35 @@
-#returns list of test vectors, in the order (seed, N) U [0, seed]
-#given arguments N inputs, Seed starting value of the counter
-#outputs test vectors to a .txt file, "TV_A.txt"
 def TV_A(N, seed):
     TV_A = []   #store TV to list
-    outputFile  = open("TV_A.txt", "w") #write TV to this file
-    idx = 0 #list index
 
-
-    for i in range(seed, pow(2,N)):
         
+    for i in range(seed, 255): 
         TV = bin(i)     #convert int to binary string
-        
-        if TV.__contains__("0b"):    #get rid of the '0b'
-            TV = TV.replace("0b", "")
+        TV = TV.replace("0b", "")   #eliminate "0b"
         
         while (len(TV) < N):
-            TV = ''.join(('0',TV))   #append with zeros if necessary
-
-
-        list.insert(TV_A, idx, TV)
-        outputFile.write(TV + "\n")
-        idx = idx + 1
-
-
-    if seed != 0:   #make sure we come back to get all N - seed vectors
-        for j in range (seed):
-            TV = bin(j)
-            if TV.__contains__("0b"):    #get rid of the '0b'
-                TV = TV.replace("0b", "")
+            TV = ''.join(('0',TV))   #pad zeros to get length 
+        
+        TV_A.append(TV)
+    
+    if seed != 0:
+        for i in range(seed):
+            TV = bin(i)     #convert int to binary string
+            TV = TV.replace("0b", "")   #eliminate "0b"
         
             while (len(TV) < N):
-                TV = ''.join(('0',TV))   #append with zeros if necessary
-
-
-            list.insert(TV_A, idx, TV)
-            outputFile.write(str(TV) + "\n")
-            idx = idx + 1
+                TV = ''.join(('0',TV))   #pad zeros to get length 
+        
+            TV_A.append(TV)
     
+    #write to txt file
+    outputFile  = open("TV_A.txt", "w") 
+    outStr = ""
+
+    for i in range(255):
+        outStr = TV_A[i][0:N]   #cut off N bits to ensure proper size
+        outputFile.write(outStr + "\n")
 
     outputFile.close()
+
 
     return TV_A
