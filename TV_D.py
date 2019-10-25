f987b00c649f92732bd951b7794f92b3eead176d
JBRUU <47766826+JBRUU@users.noreply.github.com>
2019-10-25 10:25:18 -0500

    Update TV_D.py
    completely revised to return the proper batch size, and the LFSR now uses the same seed repeated for each iteration

--- TV_D.py
+++ TV_D.py
@@ -1,7 +1,7 @@
 #LFSR is a sequence of N D-FF, with an XOR at the Nth D-FF
 
 #returns: list of 2^N test vectors of length N
-def TV_D(N, seed):
+def TV_D(N, seed, batch_low, batch_high):
     state_prev = []
     state = [] #store N states here, temporarily
     TV_D = []   #store final TV sequence here, permanently
@@ -10,33 +10,49 @@ def TV_D(N, seed):
 
     s0 = bin(seed)
     s0 = s0.replace("0b", "")   #remove "b0"
+    
+    while (len(s0) < 8):
+        s0 = ''.join(('0',s0))   #prepend with zeros to get to 8 bit length
 
-    while (len(s0) < N):
-        s0 = ''.join(('0',s0))   #append with zeros if necessary
+    for i in range (8):
+        state_prev.append(s0[i])    #get initial seed value
 
-    #populate initial seed:1
+    state = state_prev.copy()
+    TV = ""
 
-    for i in range(N):
-        state_prev.append(int(s0[i]))
-        state.append(int(s0[i]))
-
+    while len(s0) < N:
+            s0 = ''.join((s0,s0))
+    while len(s0) != N:
+        s0=s0[1:]
     TV_D.append(s0)
 
-    #perform LFSR 2
-    for i in range(pow(2,N) - seed):
-        TV_D.append("")
-        
-        for j in range(N):
+    #LFSR with XOR between two last D-FF
+    for i in range (254):
+        for j in range(8):
             if j == 0:
-                state[j] = state_prev[N-1]
-
-            #XOR operation between two last D-FF
-            if j == N -2:
-                state[j] = str(int(state_prev[N-3])^int(state_prev[N-1]))
+               
+                state[j] = state_prev[7]
+            if j == 6:
+                state[j] = str(int(state_prev[j+1])^int(state_prev[j-1]))
             else:
                 state[j] = state_prev[j-1]
-            TV_D[i+1] += str(state[j])
+            TV+=str(state[j])
         state_prev = state.copy()
 
-    print("finished!")  #debugging purposes
-    return TV_D
+        while len(TV) < N:
+            TV = ''.join((TV,TV))
+        while len(TV) != N:
+            TV=TV[1:]
+        TV_D.append(TV)
+        TV = ""
+         
+    #cut to user batch size
+    TV_D_batchCut = []
+
+    for i in range(batch_low-1,batch_high):
+        TV_D_batchCut.append(TV_D[i])
+
+
+
+
+    return TV_D_batchCut
